6c094347c28272fe6d304f8f9b36d79e2cf04e2b
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2024-12-20 10:16:50 +0100

    kleine änderung

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -7,7 +7,7 @@
       <ul>
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/users">Users</router-link></li> 
-        <li><a href="#">Bills</a></li>
+        <li><router-link to="/bills">Bills</router-link></li> 
         <li><a href="#">Tournament</a></li>
         <li><a href="#">Training Plan</a></li>
         <li><a href="#">Account</a></li>
