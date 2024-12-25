b01e85d5682b23054de42dd4c8e7d04409710000
arberidri <Arber_Idrizaj@sluz.ch>
2024-12-25 16:54:11 +0100

    sync

--- src/views/users.vue
+++ src/views/users.vue
@@ -1,9 +1,11 @@
 <script setup>
+import usertable from '../components/usertable.vue';
  </script>
 
 <template>
     <div class="users-page">
       <h1>Users Page</h1>
+      <usertable />
       <p>Welcome to the Users page!</p>
     </div>
   </template>
