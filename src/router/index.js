1bcf219edb5cfc587d7bd74107c166fd56d53946
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2025-01-10 08:16:37 +0100

    frontend login logik

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,69 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '../components/HelloWorld.vue';
+import Home from '../views/Home.vue';
 import Users from '../views/users.vue'; 
 import Bills from '../views/bills.vue';
 import Tournament from '../views/Tournament.vue';
 import Plan from '../views/Plan.vue';
 import Account from '../views/Account.vue';
+import Login from '../views/login.vue'; 
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
     path: '/',
     name: 'Home',
-    component: HelloWorld,
+    component: Home,
+    meta: { requiresAuth: true }, 
   },
   {
     path: '/users',
     name: 'Users',
     component: Users, 
+    meta: { requiresAuth: true },
   },
   {
     path: '/bills',
     name: 'Bills',
     component: Bills, 
+    meta: { requiresAuth: true },
   },
   {
     path: '/Tournament',
     name: 'Tournament',
     component: Tournament, 
+    meta: { requiresAuth: true },
   },
   {
     path: '/Plan',
     name: 'Training-Plan',
     component: Plan, 
+    meta: { requiresAuth: true },
   },
   {
     path: '/Account',
     name: 'Account',
     component: Account, 
+    meta: { requiresAuth: true },
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = localStorage.getItem('loggedIn') === 'true';
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    next('/login'); 
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,43 @@
-<script setup>
- </script>
-
 <template>
-    <div class="users-page">
-      <h1>Login Page</h1>
-      <p>Welcome to the Login page!</p>
+    <div>
+      <h1>Login</h1>
+      <form @submit.prevent="handleLogin">
+        <div>
+          <label for="username">Benutzername:</label>
+          <input id="username" v-model="username" type="text" />
+        </div>
+        <div>
+          <label for="password">Passwort:</label>
+          <input id="password" v-model="password" type="password" />
+        </div>
+        <button type="submit">Einloggen</button>
+        <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
+      </form>
     </div>
   </template>
   
-
-
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        errorMessage: '',
+      };
+    },
+    methods: {
+      handleLogin() {
+        const validUsername = 'test';
+        const validPassword = '123';
+  
+        if (this.username === validUsername && this.password === validPassword) {
+          localStorage.setItem('loggedIn', 'true'); 
+          this.$router.push('/'); 
+        } else {
+          this.errorMessage = 'Ungültige Anmeldedaten!';
+        }
+      },
+    },
+  };
+  </script>
   
