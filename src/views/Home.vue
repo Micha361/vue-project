b1d978af961424c29f3e686a993d6bf69ae2a08a
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2025-01-10 08:19:57 +0100

    update home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
- </script>
+import TournamentsSoon from '../components/TournamentsSoon.vue';
+</script>
 
 <template>
     <div class="users-page">
       <h1>Home Page</h1>
       <p>Welcome to the Home page!</p>
+      <TournamentsSoon />
     </div>
   </template>
   
