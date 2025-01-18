980a732040d784c20619431794ac943b18ed1c99
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2025-01-18 20:10:56 +0100

    account, trainingsplan seite

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,10 +2,34 @@
  </script>
 
 <template>
-    <div class="users-page">
-      <h1>Account Page</h1>
-      <p>Welcome to the Account page!</p>
-    </div>
-  </template>
-  
+  <div class="account-container">
+    <h1 class="title">Account Details</h1>
+    <div class="account-card">
+      <div class="account-detail">
+        <label class="detail-label">Username:</label>
+        <span class="detail-value">{{ username || 'Dein Benutzername' }}</span>
+      </div>
+      <div class="account-detail">
+        <label class="detail-label">Email:</label>
+        <span class="detail-value">{{ email || 'Deine Email-Adresse' }}</span>
+      </div>
+      <button class="signout" @click="signOut">Sign Out</button>
+     </div>
+  </div>
+</template>
 
+<style>
+.signout{
+  margin-top: 100px;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    signOut() {
+      localStorage.removeItem('loggedIn'); 
+      this.$router.push('/login'); 
+    },
+  },
+};</script>
