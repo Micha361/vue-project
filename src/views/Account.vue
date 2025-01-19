6b7c3637bb58f80a9f91edb28ddd5b16af1e9da1
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2025-01-19 10:37:46 +0100

    verbesserungen

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -5,13 +5,15 @@
   <div class="account-container">
     <h1 class="title">Account Details</h1>
     <div class="account-card">
+
       <div class="account-detail">
-        <label class="detail-label">Username:</label>
-        <span class="detail-value">{{ username || 'Dein Benutzername' }}</span>
-      </div>
+        <strong><label class="detail-label">Email:</label></strong>
+        <span class="detail-value">{{ email || ' test@gmail.com' }}</span>
+      </div><br>
       <div class="account-detail">
-        <label class="detail-label">Email:</label>
-        <span class="detail-value">{{ email || 'Deine Email-Adresse' }}</span>
+        <label class="detail-label">Bei Problemen mit deinem Account melde dich unter <br>
+          <a href="mailto:arber_idrizaj@sluz.ch">arber_idrizaj@sluz.ch</a> oder unter der Nummer <a href="tel:+41791234567">079 123 45 67</a>
+        </label>
       </div>
       <button class="signout" @click="signOut">Sign Out</button>
      </div>
